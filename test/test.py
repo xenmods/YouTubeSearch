57c03ff8f2a09dbd28fd52de9cdbd9970dcb5b97
Xen <70523287+xenmods@users.noreply.github.com>
2022-11-15 20:18:39 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,13 +1,13 @@
 from YouTubeSearch.youtube import YoutubeSearch
-
+import json
 
 # Search using keywords and on page 2
 search = YoutubeSearch()
-result = search.search('advanced python tutorial', page=2)
+result = search.search('advanced python tutorial', type='playlist')
 if result['success']:
-    print(result)
+    print(json.dumps(result, indent=4))
 
 # Get information of a video
 result = search.info('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 if result['success']:
-    print(result)
+    print(json.dumps(result, indent=4))
